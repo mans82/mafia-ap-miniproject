75325daf840ca3b159d65bdd2e44beecedb99b86
Mans <cando428@gmail.com>
2021-03-28 19:46:07 +0430

    Add initial implementation for Detective.java

---
+++ src/players/Detective.java
@@ -0,0 +1,30 @@
+package players;
+
+import exceptions.CannotPlayException;
+
+public class Detective extends Player{
+
+    private boolean hasQueried = false;
+
+    protected Detective(String name) {
+        super(name);
+    }
+
+    @Override
+    public void playOn(Player player) throws CannotPlayException {
+        if (this.hasQueried) {
+            throw new CannotPlayException("detective has already asked");
+        }
+        if (player.isDead()) {
+            throw new CannotPlayException("suspect is dead");
+        }
+        System.out.println(player.isMafia() ? "YES" : "NO");
+        this.hasQueried = true;
+    }
+
+    @Override
+    public void resetState() {
+        super.resetState();
+        this.hasQueried = false;
+    }
+}
