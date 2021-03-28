d605b913885c8c628512a5d8f02f47a8307bb9ed
Mans <cando428@gmail.com>
2021-03-28 20:49:17 +0430

    Add initial implementation for Silencer.java

---
+++ src/players/Silencer.java
@@ -0,0 +1,30 @@
+package players;
+
+import exceptions.CannotPlayException;
+
+public class Silencer extends Mafia{
+
+    private Player silencedPlayer = null;
+    private boolean hasSilenced = false;
+
+    protected Silencer(String name) {
+        super(name);
+    }
+
+    @Override
+    public void playOn(Player player) throws CannotPlayException {
+        if (hasSilenced) {
+            super.playOn(player);
+        } else {
+            this.silencedPlayer = player;
+            this.hasSilenced = true;
+        }
+    }
+
+    @Override
+    public void resetState() {
+        super.resetState();
+        this.silencedPlayer = null;
+        this.hasSilenced = false;
+    }
+}
