2dab21fdc088d5caac8b414aaa7b9a695a907c25
Mans <cando428@gmail.com>
2021-03-28 17:58:05 +0430

    Add initial implementation for Player.java

---
+++ src/players/Player.java
@@ -0,0 +1,39 @@
+package players;
+
+public abstract class Player {
+    private final String name;
+    private int voteCount = 0;
+    private boolean voted = false;
+    private boolean isDead = false;
+
+    protected Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void vote(Player votee) throws IllegalStateException{
+        if (this.voted) {
+            throw new IllegalStateException("Player already voted");
+        }
+        votee.voteCount++;
+        this.voted = true;
+    }
+
+    public void resetState() {
+        this.voteCount = 0;
+        this.voted = false;
+    }
+
+    public void die() {
+        this.isDead = true;
+    }
+
+    public boolean isDead() {
+        return isDead;
+    }
+
+    public abstract void playOn(Player player);
+}
