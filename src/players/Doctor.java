ffff6af5be5b652cb1386f3d78c46f27caaf09c8
Mans <cando428@gmail.com>
2021-03-28 18:36:23 +0430

    Add initial implementation for Doctor.java

---
+++ src/players/Doctor.java
@@ -0,0 +1,26 @@
+package players;
+
+
+public class Doctor extends Player{
+
+    private Player savedPlayer = null;
+
+    protected Doctor(String name) {
+        super(name);
+    }
+
+    @Override
+    public void playOn(Player player) {
+        this.savedPlayer = player;
+    }
+
+    @Override
+    public void resetState() {
+        super.resetState();
+        this.savedPlayer = null;
+    }
+
+    public Player getSavedPlayer() {
+        return savedPlayer;
+    }
+}
